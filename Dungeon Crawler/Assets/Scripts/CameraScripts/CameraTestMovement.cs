76e82a95c6d23c8ad2a30a7faf3e9f52aaa490dd
Jesper Wentzell <jesper.wentzell@gmail.com>
2024-04-05 17:33:25 +0200

    Wall detection
    The player can no longer walk through walls. Also added a few walls to the "play area". closes #38

--- Dungeon Crawler/Assets/Scripts/CameraScripts/CameraTestMovement.cs
+++ Dungeon Crawler/Assets/Scripts/CameraScripts/CameraTestMovement.cs
@@ -3,23 +3,22 @@ using UnityEngine;
 public class CameraTestMovement : MonoBehaviour
 {
     public GameObject targetObject;
+    public float moveDistance = 6.0f;
+    public LayerMask wallLayer;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("w")) // Press "w" on keyboard
         {
-            Vector3 forwardDirection = targetObject.transform.forward.normalized;  // Get the current forward direction
+            // Get the current forward direction
+            Vector3 forwardDirection = targetObject.transform.forward.normalized;  
 
-            // Check direction
-            if (forwardDirection.x > 0.5f)  // Approximately facing positive X
-                targetObject.transform.position += new Vector3(6, 0, 0);
-            else if (forwardDirection.x < -0.5f) // Approximately facing negative X 
-                targetObject.transform.position += new Vector3(-6, 0, 0);
-            else if (forwardDirection.z > 0.5f)  // Approximately facing positive Z
-                targetObject.transform.position += new Vector3(0, 0, 6);
-            else if (forwardDirection.z < -0.5f) // Approximately facing negative Z
-                targetObject.transform.position += new Vector3(0, 0, -6);
+            // Multiple raycasts for 3D collision detection
+            if (CanMove(forwardDirection))
+            {
+                targetObject.transform.position += forwardDirection * moveDistance; // Move
+            }
         }
         else if (Input.GetKeyDown("a")) // Press "a" on keyboard
         {
@@ -31,5 +30,20 @@ public class CameraTestMovement : MonoBehaviour
             // Rotate the object 90 degrees on the Y-axis
             targetObject.transform.Rotate(0f, 90f, 0f);
         }
+    }
+    private bool CanMove(Vector3 direction)
+    {
+        // Adjust number of raycasts and offset positions
+        Vector3 offset = new Vector3(0, 0.5f, 0); // Offset for slightly elevated raycasts 
+
+        /*
+         Raycasting is a lot like sending out a laser and checking if it hits something within a certain range, in this case moveDistance. 
+         The offset helps with consistency and allows for player to have a box collider.
+         It will only detect collitions in the wallLayer and ignore everything else i.e only detecting walls.
+         - Jesper
+         */
+        return !Physics.Raycast(targetObject.transform.position + offset, direction, out RaycastHit hit, moveDistance, wallLayer);
+
+
     }
 }
