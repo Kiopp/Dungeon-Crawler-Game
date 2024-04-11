e54fab5485b48a6f520fb4a963012c12222b8ab3
CarlNord01 <noca22tf@student.ju.se>
2024-04-11 14:43:03 +0200

    Add InventoryManager + InventoryButton + some logic

---
+++ Dungeon Crawler/Assets/Scripts/Classes/ItemPickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPickup : MonoBehaviour
+{
+    public Item Item;
+
+    void Pickup()
+    {
+        InventoryManager.Instance.Add(Item);
+        Destroy(gameObject);
+    }
+
+    private void OnMouseDown()
+    {
+        Pickup();
+    }
+}
