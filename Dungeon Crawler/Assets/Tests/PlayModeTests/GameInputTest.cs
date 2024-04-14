c7b416d7b0ab3798a60b70b54b530fbe5aa1dd15
Jesper Wentzell <jesper.wentzell@gmail.com>
2024-04-14 17:17:23 +0200

    More tests added
    Added continous movement test
    Added rotation tests

--- Dungeon Crawler/Assets/Tests/PlayModeTests/GameInputTest.cs
+++ Dungeon Crawler/Assets/Tests/PlayModeTests/GameInputTest.cs
@@ -51,4 +51,134 @@ public class GameInputTest
         var movedForward = Vector3.Dot(gameObject.transform.forward, endPosition - startPosition) > 0;
         Assert.IsTrue(movedForward);
     }
+
+    [UnityTest]
+    public IEnumerator MoveThreeTimes()
+    {
+        // Setup
+        var gameObject = new GameObject();
+        var player = gameObject.AddComponent<GameInput>();
+
+        player.PlayerObject = gameObject;
+
+        // Gather pre-test information
+        var startTime = Time.time;
+        var moveDelay = player.GetMoveDelay() * 3;
+        var startPosition = gameObject.transform.position;
+
+        // Enable movement
+        player.EnableMovement();
+
+        // Move
+        player.OnMoveStart();
+        yield return new WaitForSeconds(moveDelay + 0.3f); // + 0.3f for extra delay.
+        player.OnMoveRelease();
+
+        //Disable movement
+        player.DisableMovement();
+
+        // Gather post-test information
+        var endPosition = gameObject.transform.position;
+
+        // Confirm that timing is correct
+        var elapsedTime = Time.time - startTime;
+        Assert.That(elapsedTime, Is.GreaterThanOrEqualTo(moveDelay));
+
+        // Give coroutine some time to finish
+        yield return new WaitForSeconds(1);
+
+        // Confirm that object moved correct amount
+        var distanceMoved = Vector3.Distance(startPosition, endPosition);
+        Assert.That(distanceMoved, Is.EqualTo(player.GetMoveDistance() * 3).Within(0.5f));
+
+        // Confirm the direction of movement
+        var movedForward = Vector3.Dot(gameObject.transform.forward, endPosition - startPosition) > 0;
+        Assert.IsTrue(movedForward);
+    }
+
+    [UnityTest]
+    public IEnumerator RotateRightOnce()
+    {
+        // Setup
+        var gameObject = new GameObject();
+        var player = gameObject.AddComponent<GameInput>();
+        player.EnableSimulation(); // class no longer takes input from the PlayerInputActions
+        player.SetRotationValue(1f); // simulate right rotation
+        player.PlayerObject = gameObject;
+
+        // Gather pre-test information
+        var startTime = Time.time;
+        var rotationDelay = player.GetRotateDelay();
+        var startRotation = gameObject.transform.rotation;
+
+        // Enable movement
+        player.EnableMovement();
+
+        // Move
+        player.OnRotateStart();
+        yield return new WaitForSeconds(rotationDelay);
+        player.OnRotateRelease();
+
+        //Disable movement
+        player.DisableMovement();
+
+        // Gather post-test information
+        var endRotation = gameObject.transform.rotation;
+
+        // Confirm that timing is correct
+        var elapsedTime = Time.time - startTime;
+        Assert.That(elapsedTime, Is.GreaterThanOrEqualTo(rotationDelay));
+
+        // Give coroutine some time to finish
+        yield return new WaitForSeconds(1);
+
+        // Confirm any rotation
+        Assert.AreNotEqual(startRotation, endRotation);
+
+        // Confirm correct direction
+        Assert.That(endRotation.eulerAngles.y, Is.EqualTo(startRotation.eulerAngles.y + 270).Within(1f));
+    }
+
+    [UnityTest]
+    public IEnumerator RotateLeftOnce()
+    {
+        // Setup
+        var gameObject = new GameObject();
+        var player = gameObject.AddComponent<GameInput>();
+        player.EnableSimulation(); // class no longer takes input from the PlayerInputActions
+        player.SetRotationValue(-1f); // simulate right rotation
+        player.PlayerObject = gameObject;
+
+        // Gather pre-test information
+        var startTime = Time.time;
+        var rotationDelay = player.GetRotateDelay();
+        var startRotation = gameObject.transform.rotation;
+
+        // Enable movement
+        player.EnableMovement();
+
+        // Move
+        player.OnRotateStart();
+        yield return new WaitForSeconds(rotationDelay);
+        player.OnRotateRelease();
+
+        //Disable movement
+        player.DisableMovement();
+
+        // Gather post-test information
+        var endRotation = gameObject.transform.rotation;
+
+        // Confirm that timing is correct
+        var elapsedTime = Time.time - startTime;
+        Assert.That(elapsedTime, Is.GreaterThanOrEqualTo(rotationDelay));
+
+        // Give coroutine some time to finish
+        yield return new WaitForSeconds(1);
+
+        // Confirm any rotation
+        Assert.AreNotEqual(startRotation, endRotation);
+
+        // Confirm correct direction
+        Assert.That(endRotation.eulerAngles.y, Is.EqualTo(startRotation.eulerAngles.y + 90).Within(1f));
+    }
 }
