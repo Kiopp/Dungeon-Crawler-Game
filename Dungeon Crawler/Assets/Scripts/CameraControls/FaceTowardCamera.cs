6d800a4d1f945bcaadc304067d12609f985205b8
Jesper Wentzell <jesper.wentzell@gmail.com>
2024-05-10 15:03:00 +0200

    Update FaceTowardCamera.cs
    added comments in FaceTowardCamera script

--- Dungeon Crawler/Assets/Scripts/CameraControls/FaceTowardCamera.cs
+++ Dungeon Crawler/Assets/Scripts/CameraControls/FaceTowardCamera.cs
@@ -6,16 +6,21 @@ public class FacePlayer : MonoBehaviour
 {
     private Transform cameraTransform;
     [SerializeField] private Vector3 rotationOffset = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Get the transform object from the main camera
         cameraTransform = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Call built-in LookAt method
         transform.LookAt(cameraTransform);
+
+        // Adjust offset rotation
         transform.Rotate(rotationOffset);
     }
 }
