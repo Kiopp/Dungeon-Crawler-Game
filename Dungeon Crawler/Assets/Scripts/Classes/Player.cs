eb804ee0c95913612c50f3fc87bdada6c3ed843d
Vencilo <axbe701@gmail.com>
2024-05-16 16:29:10 +0200

    tried fixing whitespace error in player

--- Dungeon Crawler/Assets/Scripts/Classes/Player.cs
+++ Dungeon Crawler/Assets/Scripts/Classes/Player.cs
@@ -1,41 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
-//Represents the player entity
+
+// Represents the player entity
 public class Player : Entity
 {
-    [SerializeField] private int startHealth; //Player starting health (Visisble and editable in unity inspector)
-    [SerializeField] private int attackDamage; //Player damage (Visisble and editable in unity inspector)
-    [SerializeField] private float dodgeProbability; //Player dodge probability (Visisble and editable in unity inspector)
+    [SerializeField] private int startHealth;      // Player starting health (Visible and editable in Unity Inspector)
+    [SerializeField] private int attackDamage;     // Player damage (Visible and editable in Unity Inspector)
+    [SerializeField] private float dodgeProbability;  // Player dodge probability (Visible and editable in Unity Inspector)
 
-    //Overrides the start method to initialize the enemy health and damage
+    // Overrides the Start method to initialize the player health and damage
     protected override void Start()
     {
-        base.Start(); //Calls the base class start to initialize the currenthealth
-        Health = startHealth; //Sets the enemy starting health
-        Damage = attackDamage; //Sets the enemy damage
+        base.Start();  // Calls the base class start to initialize the current health
+        currentHealth = startHealth;  // Sets the enemy health to the player starting health
+        Damage = attackDamage;  // Sets the player damage
     }
 
-    //Attacks an enemy
+    // Attacks an enemy
     public override void Attack(IBattleEntity enemy)
     {
-        enemy.TakeDamage(Damage); //Inflicts the player damage to an enemy
+        enemy.TakeDamage(Damage);  // Inflicts the player damage to an enemy
     }
 
-    //Inflicts damage to the player
+    // Inflicts damage to the player
     public override void TakeDamage(int Damage)
     {
-        if (Random.Range(0F, 1F) >= dodgeProbability) //Has a chance to randomly dodge an attack
+        if (Random.Range(0F, 1F) >= dodgeProbability)  // Has a chance to randomly dodge an attack
         {
-            Health -= Damage; //Reduces the player health by the damage dealt
+            currentHealth -= Damage;  // Reduces the player health by the damage dealt
         }
     }
 
-    //Checks if the player is dead
+    // Checks if the player is dead
     public override bool Dead()
     {
-        return Health <= 0; //Returns true if the player is dead
+        return currentHealth <= 0;  // Returns true if the player is dead
     }
-
 }
