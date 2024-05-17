e301576d6e09aa3afb36d7aec681ef095eb07baa
Vencilo <axbe701@gmail.com>
2024-05-17 13:24:53 +0200

    Fixed some indentation and whitespace errors

--- Dungeon Crawler/Assets/Scripts/Inventory/Item.cs
+++ Dungeon Crawler/Assets/Scripts/Inventory/Item.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [CreateAssetMenu(fileName = "New Item", menuName = "Item/Create New Item")]
-
 public class Item : ScriptableObject
 {
-    [SerializeField] private int id; //Item ID
+    [SerializeField] private int id; // Item ID
     [SerializeField] private string itemName; // Name of the item
     [SerializeField] private int Value; // Monetary value of the item
     [SerializeField] private Sprite icon;
     [SerializeField] private string description;
+
     [SerializeField]
-    private enum ItemRarity // different item rarities and their corresponding modifier values
+    private enum ItemRarity // Different item rarities and their corresponding modifier values
     {
         Common = 100,
         Uncommon = 125,
@@ -21,6 +20,7 @@ public class Item : ScriptableObject
         Epic = 175,
         Legendary = 200
     }
+
     [SerializeField] private ItemRarity Rarity; // Rarity of the item 
 
     public string GetName
@@ -40,4 +40,3 @@ public class Item : ScriptableObject
         return Modifier;
     }
 }
-   
