6d4625dce97bb967eb9e4f24ac019a0f77f78c38
mowaab <mohammadabbas@live.se>
2024-05-29 18:03:06 +0200

    Added missing part to the unit test

--- Dungeon Crawler/Assets/Tests/PlayModeTests/ItemTest.cs
+++ Dungeon Crawler/Assets/Tests/PlayModeTests/ItemTest.cs
@@ -3,25 +3,30 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 
+[TestFixture]
 public class ItemTests
 {
+    private Item testItem;
+
+    [SetUp]
+    public void SetUp()
+    {
+        testItem = ScriptableObject.CreateInstance<Item>();
+    }
 
     [Test]
     public void RarityModifier_ReturnsCorrectModifier()
     {
         // Arrange
-        var item = ScriptableObject.CreateInstance<Item>();
-
-        // Private field access through reflection
         var rarityField = typeof(Item).GetField("Rarity", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
         var itemRarityEnum = typeof(Item).GetNestedType("ItemRarity", System.Reflection.BindingFlags.NonPublic);
-        var rarityValue = System.Enum.Parse(itemRarityEnum, "Epic"); 
-        rarityField.SetValue(item, rarityValue);
+        var rarityValue = System.Enum.Parse(itemRarityEnum, "Epic");
+        rarityField.SetValue(testItem, rarityValue);
 
         var expectedModifier = 1.5;
 
         // Act
-        var actualModifier = item.RarityModifier();
+        var actualModifier = testItem.RarityModifier();
 
         // Assert
         Assert.AreEqual(expectedModifier, actualModifier);
